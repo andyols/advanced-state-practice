d5da2530129bc5618df10922acd8e70c1ea8a65c
Andy Olson <andyolson09@gmail.com>
2019-03-19 20:17:17 -0500

    reviews component

---
+++ src/components/Reviews.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Reviews extends Component {
+  state = {
+    showReviews: false
+  }
+
+  toggleReviews = () => this.setState({ showReviews: !this.state.showReviews })
+
+  render() {
+    const { product } = this.props
+
+    return (
+      <div>
+        <button
+          onClick={() => this.toggleReviews()}
+          className="btn btn-primary"
+        >
+          Reviews
+        </button>
+        <ul className="list-group">
+          {this.state.showReviews &&
+            product.reviews.map((review, i) => (
+              <li key={i} className="list-group-item">
+                {review.description}
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Reviews
